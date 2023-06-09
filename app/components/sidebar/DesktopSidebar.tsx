a05df97ba37b41bc44ed0ad809e2db84a62d4a41
codewithmawais <connectwithmawais>
2023-06-09 05:26:18 +0500

    Implemented Settings, Delete functionality and also completed UI of both and Modal component.

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import DesktopItem from "./DesktopItem";
 import Avatar from "../Avatar";
 import { User } from "@prisma/client";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
     currentUser: User;
@@ -17,46 +18,53 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="
-            hidden
-            lg:fixed
-            lg:inset-y-0
-            lg:left-0
-            lg:z-40
-            lg:w-20
-            xl:px-6
-            lg:overflow-y-auto
-            lg:bg-white
-            lg:border-r-[1px]
-            lg:pb-4
-            lg:flex
-            lg:flex-col
-            justify-between
-        
-        ">
-            <nav className="mt-4 flex flex-col justify-between">
-                <ul role="list" className="flex flex-col items-center space-y-1">
-                    {routes.map((item) => (
-                        <DesktopItem
-                            key={item.label}
-                            href={item.href}
-                            label={item.label}
-                            icon={item.icon}
-                            active={item.active}
-                            onClick={item.onClick}
-                        />
-                    ))}
-                </ul>
-            </nav>
-            <nav className="mt-4 flex flex-col justify-between items-center">
-                <div
-                    onClick={() => setIsOpen(true)}
-                    className="cursor-pointer hover:opacity-75 transition"
-                >
-                    <Avatar user={currentUser} />
-                </div>
-            </nav>
-        </div>
+        <>
+            <SettingsModal 
+                currentUser={currentUser} 
+                isOpen={isOpen} 
+                onClose={() => setIsOpen(false)} 
+            />
+            <div className="
+                hidden
+                lg:fixed
+                lg:inset-y-0
+                lg:left-0
+                lg:z-40
+                lg:w-20
+                xl:px-6
+                lg:overflow-y-auto
+                lg:bg-white
+                lg:border-r-[1px]
+                lg:pb-4
+                lg:flex
+                lg:flex-col
+                justify-between
+            
+            ">
+                <nav className="mt-4 flex flex-col justify-between">
+                    <ul role="list" className="flex flex-col items-center space-y-1">
+                        {routes.map((item) => (
+                            <DesktopItem
+                                key={item.label}
+                                href={item.href}
+                                label={item.label}
+                                icon={item.icon}
+                                active={item.active}
+                                onClick={item.onClick}
+                            />
+                        ))}
+                    </ul>
+                </nav>
+                <nav className="mt-4 flex flex-col justify-between items-center">
+                    <div
+                        onClick={() => setIsOpen(true)}
+                        className="cursor-pointer hover:opacity-75 transition"
+                    >
+                        <Avatar user={currentUser} />
+                    </div>
+                </nav>
+            </div>
+        </>
     );
 }
 
